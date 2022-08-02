9804628448ef9cfcfc36232351781a5a3d385232
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-08-01 23:15:27 -0400

    added get requests, favorites pending, fix relationships

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
-from models import db
+from models import db, Characters, Planets, User
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -14,9 +14,45 @@ db.init_app(app)
 Migrate(app,db)
 CORS(app)
 
-@app.route('/')
+#Root Route
+@app.route('/', methods=['GET'])
 def root():
     return jsonify({"msg" : "API REST FLASK"},200)
+
+#Get ALL Characters
+@app.route('/people', methods=['GET'])
+def get_people():
+    characters = Characters.query.all()
+    characters = list(map(lambda character: character.serialize(),characters))
+
+    return jsonify(characters),200
+
+#Get ONE Character by ID
+@app.route('/people/<int:id>', methods=['GET'])
+def get_people_by_id(id):
+    character = Characters.query.get(id)
+    return jsonify(character.serialize()),200
+
+#Get ALL Planets
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planets.query.all()
+    planets = list(map(lambda planet: planet.serialize(),planets))
+    return jsonify(planets),200
+
+#Get ONE Planet by ID
+@app.route('/planets/<int:id>', methods=['GET'])
+def get_planet_by_id(id):
+    planet = Planets.query.get(id)
+    return jsonify(planet.serialize()),200
+
+#Get ALL Users
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users = list(map(lambda user: user.serialize_with_favorites(),users))
+    return jsonify(users),200
+
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -5,19 +5,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String(255), unique=True, nullable=False)
+    username = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), unique=True,nullable=False)
-    favorites = db.relationship('Favorites', backref="user", uselist=False) # One to One
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    favorites = db.relationship('Favorites', backref="user")  # One to One
 
     def serialize(self):
         return{
-            "id" : self.id,
-            "username" : self.username,
-            "email" : self.email
+            "id": self.id,
+            "username": self.username,
+            "email": self.email
         }
 
     def serialize_with_favorites(self):
@@ -26,6 +27,116 @@ class User(db.Model):
             "username": self.username,
             "favorites": self.get_favorites()
         }
+
+    def get_favorites(self):
+        return list(map(lambda favorite:{'id': favorite.id, "planet_id": favorite.planet_id, "character_id": favorite.character_id, "vehicle_id": favorite.vehicle_id},self.favorites))
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))  # Foreign Key of Planets Table
+    planets = db.relationship('Planets', backref='favorites',secondary='planets')  # One to Many
+    character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))  # Foreign Key of Characters Table
+    characters = db.relationship('Characters', backref='favorites', secondary='characters')  # One to Many
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicles.id'))  # Foreign Key of Vehicles Table
+    vehicles = db.relationship('Vehicles', backref='favorites', secondary='vehicles')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # Foreign Key of User Table
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    gravity = db.Column(db.String(255))
+    terrain = db.Column(db.String(255))
+    population = db.Column(db.Integer)
+
+    def serialize(self):
+        return{
+            "id" :  self.id,
+            "name" : self.name,
+            "gravity" : self.gravity,
+            "terrain" : self.terrain,
+            "population" : self.population
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Characters(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    height = db.Column(db.String(255))
+    mass = db.Column(db.String(255))
+    gender = db.Column(db.String(255))
+    # Foreign Key of Vehicles Table , ONE TO MANY
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicles.id'))
+    char_vehicles = db.relationship('Vehicles', backref='characters')
+
+    def serialize(self):
+        return{
+            "id" : self.id,
+            "name" : self.name,
+            "height" : self.height,
+            "mass" : self.mass,
+            "gender" : self.gender,
+            "vehicle_id" : self.vehicle_id
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Vehicles(db.Model):
+    __tablename__ = 'vehicles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    model = db.Column(db.String(255))
+    cost = db.Column(db.Integer)
+    max_speed = db.Column(db.Integer)
+    pilots = db.Column(db.Integer)
 
     def save(self):
         db.session.add(self)
@@ -37,45 +148,3 @@ class User(db.Model):
     def delete(self):
         db.session.delete(self)
         db.session.commit()
-
-class Favorites(db.Model):
-    __tablename__ = 'favorites'
-    id = db.Column(db.Integer,primary_key=True)
-    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id')) # Foreign Key of Planets Table
-    planets = db.relationship('Planets',backref= 'favorites') #  One to Many
-    character_id = db.Column(db.Integer, db.ForeignKey('characters.id')) # Foreign Key of Characters Table
-    characters = db.relationship('Characters',backref= 'favorites')#  One to Many
-    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicles.id')) # Foreign Key of Vehicles Table
-    vehicles = db.relationship('Vehicles',backref='favorites')
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # Foreign Key of User Table
-
-class Planets(db.Model):
-    __tablename__ = 'planets'
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(255))
-    gravity = db.Column(db.String(255))
-    terrain = db.Column(db.String(255))
-    population = db.Column(db.Integer)
-
-class Characters(db.Model):
-    __tablename__ = 'characters'
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(255))
-    height = db.Column(db.String(255))
-    mass = db.Column(db.String(255))
-    gender = db.Column(db.String(255))
-    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicles.id'))# Foreign Key of Vehicles Table , ONE TO MANY
-    char_vehicles = db.relationship('Vehicles',backref='characters')
-
-class Vehicles(db.Model):
-    __tablename__ = 'vehicles'
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(255))
-    model = db.Column(db.String(255))
-    cost = db.Column(db.Integer)
-    max_speed = db.Column(db.Integer)
-    pilots = db.Column(db.Integer)
-
-
-
-
